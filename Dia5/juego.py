d194f8a6283b8039df67e4c01e62cdc35f802c02
Valentana <alumno29.tana.valentino@ipm.edu.ar>
2026-09-24 11:43:07 -0300

    juego de luz

--- Dia5/juego.py
+++ Dia5/juego.py
@@ -3,44 +3,79 @@ import board
 import neopixel
 import random
 import RPi.GPIO as GPIO
-a=-1
-c=random.randint(0,15)
+
+w=0
 boton= 21
-vel=1
+vel=0.5
 pixel_pin = board.D18
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(boton,GPIO.IN)
-
-num_pixels = 16 # Número de LEDs en tu tira
-ORDER = neopixel.GRB # Orden de colores (A veces es RGB)
+GPIO.setup(boton, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+num_pixels = 16 
+ORDER = neopixel.GRB 
 
 pixels = neopixel.NeoPixel(
     pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
 )
-try:
-    while True: 
-            pixels[a]=(0,0,0)
-            time.sleep(vel)
-            a=a+1
-            pixels[a]=(255,0,0)
-            if a==15:
-                 a=-1
-            
-            pixels[c]=(6,7,67)
-            
-            if boton==True and a==c:
-                vel=vel-0.2
-                c=random.randint(0,15)    
-            
-            if vel==0:
-                for i in range(5):
-                    pixels[0,1,2,3,4,5]=(4,4,4)
-                    pixels.show()
-                    time.sleep(0,5)
-                    pixels[0,1,2,3,4,5]=(0,0,0)
-                    pixels.show()
-
+def gano():
+    for i in range (4):
+        pixels.fill((0,255,0))
+        pixels.show()
+        time.sleep(0.5)
+        pixels.fill((0,0,0))
+        pixels.show()
+        time.sleep(0.5)
+    global w
+    w=1
+    global vel
+    vel=vel-0.1
+    if vel==0:
+        for i in range (4):
+            pixels.fill((0,0,0))
+            pixels.fill((0,0,255))
             pixels.show()
+            time.sleep(0.5)
+            pixels.fill((0,0,0))
+            pixels.show()
+            time.sleep(0.5)
+        vel=0.5
+        w=1
+
+def perdio():
+    for i in range(4):
+        pixels.fill((255,0,0))
+        pixels.show()
+        time.sleep(0.5)
+        pixels.fill((0,0,0))
+        pixels.show()
+        time.sleep(0.5)
+        
+    global vel
+    vel=0.5
+    global w
+    w=1
+
+try:
+    while True:
+        w=0
+        c=random.randint(0,15)
+        while w==0: 
+            for i in range(num_pixels):
+                pixels[i]=(5,5,5)
+                pixels[c]=(255,25,0)
+                pixels.show()
+                
+                if GPIO.input(boton)==False:
+                    if (i-1)==c:
+                        gano()
+                        break
+                    else:
+                        perdio()
+                        break
+                time.sleep(vel)
+                pixels[i]=(0,0,0)
+                pixels.show()
+
 except KeyboardInterrupt:
     pixels.fill((0, 0, 0))
     pixels.show()
